1784390aaef5e196b56787d6a6e41d256b17772c
m0 <m0@viper.internal.network>
2009-08-30 12:21:06 +0100

    Regenerate.

--- insthier.c
+++ insthier.c
@@ -14,6 +14,8 @@ struct install_item insthier[] = {
   {INST_COPY, "serial_io.adb", 0, ctxt_incdir, 0, 0, 0644},
   {INST_COPY, "serial_io.ali", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "serial_io.ali", 0, ctxt_incdir, 0, 0, 0444},
+  {INST_COPY, "serial_io.sld", 0, ctxt_repos, 0, 0, 0644},
+  {INST_COPY, "serial_io.a", "libserial_io.a", ctxt_slibdir, 0, 0, 0644},
   {INST_COPY, "serial_io-conf.ld", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY_EXEC, "serial_io-conf", 0, ctxt_bindir, 0, 0, 0755},
 };
